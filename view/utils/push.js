479dc4856267f20139d505c086091f2d68c845d8
Issa Chanzi <s3783615@student.rmit.edu.au>
2025-01-10 17:23:10 +1100

    Add dependencied for react native firebase

---
+++ view/utils/push.js
@@ -0,0 +1,15 @@
+import firebase from '@react-native-firebase/messaging';
+
+export function setup () {
+    (async () => {
+
+    }) ();
+}
+
+export async function token () {
+    const messaging = firebase.messaging ();
+
+
+}
+
+
