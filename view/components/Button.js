328ff7220b1ab99b2cfbd501de59cf5cf916e217
Issa Chanzi <s3783615@student.rmit.edu.au>
2025-01-13 14:12:24 +1100

    Add highlight prop to Button

--- view/components/Button.js
+++ view/components/Button.js
@@ -7,10 +7,16 @@ import {
     TouchableWithoutFeedback, View,
 } from 'react-native';
 import {useState} from 'react';
-import {BG_PRIMARY, BG_SECONDARY, BG_SYSTEM, FG_PRIMARY, FG_SECONDARY} from '../Colors';
+import {BG_PRIMARY, BG_SECONDARY, BG_SYSTEM, FG_HIGHIGHT, FG_PRIMARY, FG_SECONDARY} from '../Colors';
 import DropShadow from 'react-native-drop-shadow';
 
-export default function Button ({ text, onClick, isPrimary = false, style}) {
+export default function Button ({
+        text,
+        onClick,
+        isPrimary = false,
+        highlight = false,
+        style
+}) {
     const [isPressed, setIsPressed] = useState(false);
 
     const handlePressIn = () => setIsPressed (true);
@@ -24,7 +30,12 @@ export default function Button ({ text, onClick, isPrimary = false, style}) {
         isPrimary ? styles.primary : styles.secondary
     );
 
-    const buttonStyle = [styles.button, primaryStyle, style];
+    const highlightStyle = highlight ? {
+        borderColor: FG_HIGHIGHT,
+        borderWidth: 2,
+    } : {};
+
+    const buttonStyle = [styles.button, primaryStyle, style, highlightStyle];
     const viewStyle = [styles.view, primaryStyle];
     const textStyle = [styles.text, primaryStyle];
 
